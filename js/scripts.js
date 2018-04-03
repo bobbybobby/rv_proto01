23f3725f3c9608fcf10c46f0461753e646eea617
Quentin <qrichaud@gmail.com>
2018-04-03 23:57:38 +0200

    test circle buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(".card-triangle-button").on("click", function () {
+$(".flip-card-buttons .button").on("click", function () {
 
-    choice = $(this).parent().attr("class");
+    let validateClicked = $(this).hasClass('yes');
 
     $(".card-flipper").css("transform", "rotateY(0deg)");
 
     $(this).closest(".card").find(".card-back-left").show();
     $(this).closest(".card").find(".card-back-right").show();
 
-    if (choice === "card-button-left") {
-        $(this).closest(".card").find(".card-back-right").hide();
-        $(this).closest(".card").find(".card-flipper").css("transform", "rotateY(-180deg)");
-    } else if (choice === "card-button-right") {
+    if (validateClicked) {
         $(this).closest(".card").find(".card-back-left").hide();
         $(this).closest(".card").find(".card-flipper").css("transform", "rotateY(180deg)");
+    } else {
+        $(this).closest(".card").find(".card-back-right").hide();
+        $(this).closest(".card").find(".card-flipper").css("transform", "rotateY(-180deg)");
     }
     
 });
